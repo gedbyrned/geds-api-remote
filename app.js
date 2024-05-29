dabc9efa1c4a036b30e05b0e878f0222867ac435
Gerard Byrne <gedbyrned@gmail.com>
2024-05-29 10:57:31 +0100

    article id and appropriate error handling

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
-const express = require('express')
-const app = express()
-const { getTopics, getEndpoints } = require('./controllers/topics.controller')
+const express = require('express');
+const app = express();
+const { getTopics, getEndpoints, getArticleId } = require('./controllers/get.controller')
 
 app.use(express.json());
 
 app.get('/api', getEndpoints);
+
 app.get('/api/topics', getTopics);
 
-app.all('*', (req, res) => {
+app.get('/api/articles/:article_id', getArticleId);
+
+app.all('*/*', (req, res) => {
     res.status(404).send({msg: "404: route not found"})
+      });
+
+app.use((err, req, res, next) => {
+        res.status(400).send({msg: 'Bad request'});
 });
 
-module.exports = app
+
+
+
+module.exports = app;
--- models/get.model.js
+++ models/get.model.js
@@ -7,4 +7,10 @@ exports.selectTopics = () => {
     })
 };
 
+exports.selectArticleId = (article_id) => {
+    return db.query(`SELECT * FROM articles WHERE article_id = $1;`, [article_id])
+    .then((result) => {
+        return result.rows[0]
+    })
+}
 
