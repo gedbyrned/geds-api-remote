4604e8006cae4fd7b01eedcc596ea1c3b2fb293f
Gerard Byrne <gedbyrned@gmail.com>
2024-05-30 16:30:51 +0100

    delete comment from id

--- models/get.model.js
+++ models/get.model.js
@@ -80,3 +80,19 @@ exports.updateVotes = (article_id, inc_votes) => {
 
 }
 
+exports.selectCommentId = (comment_id) => {
+    return db.query(
+        `DELETE FROM comments 
+         WHERE comment_id = $1 
+         RETURNING *;`,
+        [comment_id]
+    )
+    .then((result) => {
+
+    if (result.rowCount === 0) {
+         return Promise.reject(
+        { status: 404, 
+        msg: `404: comment id number ${comment_id} is not valid` });
+        }
+    });
+}
