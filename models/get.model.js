02a72dac093e7512a67858efa4d60af5670701b0
Gerard Byrne <gedbyrned@gmail.com>
2024-05-31 11:52:24 +0100

    comment count for articleid

--- models/get.model.js
+++ models/get.model.js
@@ -9,7 +9,14 @@ exports.selectTopics = () => {
 };
 
 exports.selectArticleId = (article_id) => {
-    return db.query(`SELECT * FROM articles WHERE article_id = $1;`, [article_id])
+    return db.query
+    (`SELECT articles.author, articles.title, articles.article_id, articles.body, articles.topic, articles.created_at, articles.votes, articles.article_img_url,
+    COUNT(*) 
+    AS comment_count
+    FROM articles
+    LEFT JOIN comments ON articles.article_id = comments.article_id
+    WHERE articles.article_id = $1
+    GROUP BY articles.article_id;`, [article_id])
     .then((result) => {
         if (result.rows.length === 0) {            
             return Promise.reject({
--- psql-playground.sql
+++ psql-playground.sql
@@ -2,3 +2,10 @@
 
 SELECT topic FROM articles;
 
+SELECT articles.author, articles.title, articles.article_id, articles.body, articles.topic, articles.created_at, articles.votes, articles.article_img_url,
+    COUNT(comments.article_id) 
+    AS comment_count
+    FROM articles
+    LEFT JOIN comments ON articles.article_id = comments.article_id
+    WHERE articles.article_id = 3
+    GROUP BY articles.article_id;
