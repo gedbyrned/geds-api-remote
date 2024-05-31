bb71ff411274233f50d346105718c0e33d7c06fa
Gerard Byrne <gedbyrned@gmail.com>
2024-05-31 12:21:47 +0100

    hosting changes

--- db/connection.js
+++ db/connection.js
@@ -7,8 +7,18 @@ require('dotenv').config({path: pathToEnvFile});
 
 const PGDATABASE = process.env.PGDATABASE;
 
-if (!process.env.PGDATABASE) {
-  throw new Error('PGDATABASE not set');
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error('PGDATABASE or DATABASE_URL not set');
 }
 
 module.exports = new Pool();
+
+
+const config = {};
+
+if (ENV === 'production') {
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
+}
+
+module.exports = new Pool(config);
